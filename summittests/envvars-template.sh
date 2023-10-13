b02676c0783bd3ab5ee0f720e7e3bf28b22fdfd2
William O'Mullane <womullan@lsstcam-dc01.cp.lsst.org>
2023-10-13 20:26:09 +0000

    remove keys

---
+++ summittests/envvars-template.sh
@@ -0,0 +1,8 @@
+# Source this 
+# fill in the correct vals and call this envvars.sh
+
+export S3_ENDPOINT_URL="https://s3dfrgw.slac.stanford.edu"
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
+export bucket="rubin-summit-users"
+export prefix="testx-lhn"
