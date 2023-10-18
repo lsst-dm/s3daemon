9d29aa0aeb0b004edcffd2cd3c8657f2e0013640
William O'Mullane <womullan@lsst.org>
2023-10-18 10:13:45 -0300

    clean

--- summittests/report.py
+++ summittests/report.py
@@ -1,11 +1,9 @@
-import numpy
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
 import sys
 import re
 
-from typing import List, Dict, TextIO
+from typing import Dict, TextIO
 
 FIELDRE = re.compile(r'INFO (.+) __main__ - ([0-9]+.[0-9]+) ([0-9]+.[0-9]+)')
 
@@ -23,9 +21,9 @@ def oneplot(host, times):
 def plot(results):
     alltimes = []
     for host, timesall in results.items():
-         times = [float(t[2]) for t in timesall]
-         alltimes.append(times)
-         oneplot(host, times)
+        times = [float(t[2]) for t in timesall]
+        alltimes.append(times)
+        oneplot(host, times)
     oneplot("all", alltimes)
 
 
@@ -36,12 +34,13 @@ def load(host: str, fin: TextIO, results: Dict) -> Dict:
             continue
         fields = FIELDRE.match(line)
         if not fields:
-            print (f"Could not parse {line} for {host}", sys.stderr)
+            print(f"Could not parse {line} for {host}", sys.stderr)
             continue
         time = [fields.group(1), fields.group(2), fields.group(3)]
         times.append(time)
     results[host] = times
     return results
+
 
 def process(files: list) -> Dict:
     results = {}
@@ -49,14 +48,15 @@ def process(files: list) -> Dict:
         name = re.compile(r'.*/(lsstcam-dc[0,1][0-9])-.*')
         match = name.match(f)
         if not match:
-            print (f"Could not get host from {f}", sys.stderr)
+            print(f"Could not get host from {f}", sys.stderr)
             exit
         host = match.groups()[0]
-        print (host)
-        with open(f,'r') as fin:
+        print(host)
+        with open(f, 'r') as fin:
             load(host, fin, results)
 
     return results
+
 
 if __name__ == '__main__':
     files = sys.argv[1:]
