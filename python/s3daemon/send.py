3dccafc1e2ea33fabed14db977b87800d890aba1
Cristian Silva <cristiansilva@gmail.com>
2023-12-20 09:40:37 -0300

    fix sort of imports

--- python/s3daemon/send.py
+++ python/s3daemon/send.py
@@ -19,9 +19,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import os
 import socket
 import sys
-import os
 
 PORT = int(os.environ["S3DAEMON_PORT"])
 
